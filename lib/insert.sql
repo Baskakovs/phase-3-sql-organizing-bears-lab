c6eb98e186d80245a2d9217f9a8a1bdd99369596
Baskakovs <baskakovs@me.com>
2022-11-09 17:56:54 +0000

    commit tree 5dccc17b249f5f4fb3875ae8a008d87f6d70f1a2

---
+++ lib/insert.sql
@@ -0,0 +1,60 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(
+'Mr. Chocolate',
+12,
+'M',
+'Brown',
+'Happy',
+True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(
+'Rowdy',
+12,
+'M',
+'Brown',
+'Happy',
+True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(
+'Tahiba',
+12,
+'M',
+'Brown',
+'Happy',
+True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(
+'Sergeant Brown',
+12,
+'M',
+'Brown',
+'Happy',
+True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(
+'Melissa',
+12,
+'M',
+'Brown',
+'Happy',
+True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(
+'Grinch',
+12,
+'M',
+'Brown',
+'Happy',
+True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(
+'Wendy',
+12,
+'M',
+'Brown',
+'Happy',
+True);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES(
+12,
+'M',
+'Brown',
+'Happy',
+True)
